677d506a32e27c3297b76bc358cb6165b93de773
Tod E. Kurt <tod@todbot.com>
2013-06-15 14:43:41 -0700

    sync up

--- blink1control-test2/blink1pattern.h
+++ blink1control-test2/blink1pattern.h
@@ -4,26 +4,47 @@
 #include <QObject>
 #include <QList>
 #include <QColor>
-
+//#include <QByteArray>
+#include <QJsonObject>
 
 class Blink1Pattern : public QObject
 {
     Q_OBJECT
 
+     Q_PROPERTY(QString name READ name WRITE setName)
+     Q_PROPERTY(int repeats READ repeats WRITE setRepeats)
+     Q_PROPERTY(int playcount READ playcount WRITE setPlaycount)
+
+
 public:
-    //Blink1Pattern(QString name);
-    Blink1Pattern(QObject *parent=0); //,char *name=0 );
-    //~Blink1Pattern();
+    Blink1Pattern(QObject *parent=0);
+    Blink1Pattern(const char* name, QObject *parent=0);
+
+    QJsonObject toJson();
+    void fromJson(const QJsonObject& obj);  // make class method?
+
+    QString name() const;
+    void setName(const QString& name);
+
+    int repeats();
+    void setRepeats(int r);
+    int playcount();
+    void setPlaycount(int p);
 
     void stop();
     void play();
     void update();
 
-    QString name;
-    int repeats;
-    int playcount;
-    int playpos;
-    bool playing;
+    void resetObj();
+
+    QString patternStr();
+
+private:
+    QString mname;
+    int mrepeats;
+    int mplaycount;
+    int mplaypos;
+    bool mplaying;
 
     QList<QColor> colors;
     QList<float> times;
